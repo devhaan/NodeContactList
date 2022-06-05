64ef2b6dcc50958aacac20ef238700526aea572a
devhaan <devemahore@gmail.com>
2022-06-05 16:22:55 +0530

    contacts add via form

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set("views",path.join(__dirname, 'views'));
 
+// adding parser in res we have details in that we have out data in header we will add data to in res to handle storing in body..
+app.use(express.urlencoded());
+
 //initializing some dummy contact listeners
 let contactList =[
     {
@@ -37,8 +40,21 @@ app.get('/', (req, res) => {
        title:"Devendra Mahor",
        contact_list:contactList
     });
-})
+});
 
+app.get('/profile', function(req, res){
+    return res.render('profile', {
+        title: "Let us play with ejs"
+    });
+});
+
+//responding request for data storage post request from input form
+app.post('/create-contact', function(req, res){
+     console.log(req.body);
+    contactList.push(req.body);
+
+    return res.redirect('back'); // will redirect to same page from where request came
+});
 
 
 // listen the request on port 8000
