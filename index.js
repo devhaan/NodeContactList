a5e6f4c154fe36c5b57ac1d2eef6f127bb5674d2
devhaan <devemahore@gmail.com>
2022-06-05 13:18:39 +0530

    seting view engine as ejs and set path for views folder

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express'); // include express
 const port = 8000;
+const path = require('path');
 
 // fire express...
 
 const app = express();
+
+//add view engine ye help krta h html file ko lane me
+app.set('view engine', 'ejs');
+app.set("views",path.join(__dirname, 'views'));
+
+// resending requests back to server
+app.get('/', (req, res) => {
+
+   return res.render("home");
+})
 
 
 // listen the request on port 8000
